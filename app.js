82a42a1cbf510a969e9cefbbbb516ece9cce4510
Pablo Vasquez <vascor.pablo@gmail.com>
2023-11-04 15:23:09 -0300

    cambiando id casilla a class casilla2

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 const tablero = document.getElementById('tablero');
+const fichas = document.getElementsByClassName('ficha');
+const casilla = document.getElementsByClassName('casilla');
+
+console.log(casilla)
+
+
+for(let ficha of fichas) {
+    ficha.addEventListener("dragstart", function(e) {
+        let seleccionado = e.target;
+    
+    tablero.addEventListener("dragover", function(e) {
+        e.preventDefault();
+    });
+
+    tablero.addEventListener("drop", function(e){
+        casilla.appendChild(seleccionado);
+        seleccionado = null;
+    })
+    
+
+    })
+
+}
